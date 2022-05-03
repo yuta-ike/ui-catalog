ef87dc18bc9a298ca64f7f0c9906025f46b8fd71
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-05-04 01:47:21 +0900

    add ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,49 @@
 import type { AppProps } from "next/app"
 import { NotificationsProvider } from "@mantine/notifications"
 import "../styles/global.css"
+import Script from "next/script"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import { GA_TRACKING_ID, pageview } from "../lib/gtag"
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url)
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
+    router.events.on("hashChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+      router.events.off("hashChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
   return (
-    <NotificationsProvider>
-      <Component {...pageProps} />
-    </NotificationsProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <NotificationsProvider>
+        <Component {...pageProps} />
+      </NotificationsProvider>
+    </>
   )
 }
 
